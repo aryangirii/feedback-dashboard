92265b6b068a7a3371c2b4ac021e95d240d49941
Aryan Giri <aryangiri0915@gmail.com>
2025-06-21 11:31:10 +0530

    Fixed API URL to fetch feedback

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
-
 export async function fetchFeedback() {
-  const response = await fetch("https://5zke0356zl.execute-api.us-east-1.amazonaws.com/prod/analyze");
+  const response = await fetch("https://5zke0356zl.execute-api.us-east-1.amazonaws.com/prod/feedback");
   if (!response.ok) throw new Error("Network response was not ok");
   return await response.json();
 }
+
