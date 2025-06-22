4ec884a6b36421efe3b5db19b752772423a9dc98
Aryan Giri <aryangiri0915@gmail.com>
2025-06-22 12:28:00 +0530

    Updated feedback API endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,5 @@
-// src/api.js
 export async function fetchFeedback() {
-  const response = await fetch("https://5zke0356zl.execute-api.us-east-1.amazonaws.com/prod/feedback");
+  const response = await fetch("https://5zke0356zl.execute-api.us-east-1.amazonaws.com/prod/getFeedback");
   if (!response.ok) throw new Error("Network response was not ok");
   return await response.json();
 }
-
